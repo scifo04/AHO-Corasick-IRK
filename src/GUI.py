e4cf148cd4ba5f9413c2fd2847caf84821e32ecb
Marvin Scifo <scifohutahaean@gmail.com>
2024-08-02 21:25:11 +0700

    img: edits

--- src/GUI.py
+++ src/GUI.py
@@ -2,8 +2,10 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
 from AHOCorasick import AHOCorasick
+from PIL import Image, ImageTk
 from typing import List
 from Canvas import LeGraph
+from tkinter import PhotoImage
 import os
 
 class MainPage:
@@ -17,6 +19,11 @@ class MainPage:
     file_path: str
     file_name: str
     file_label: ttk.Label
+    bg_label: ttk.Label
+    bg_image: PhotoImage
+    upload_image: ImageTk.PhotoImage
+    solve_image: ImageTk.PhotoImage
+    visualize_image: ImageTk.PhotoImage
     text_output: tk.Text
     solution_pack: AHOCorasick
     output_content: List[str]
@@ -25,9 +32,10 @@ class MainPage:
     def __init__ (self):
         self.root = tk.Tk()
         self.root.title("AHO-Corasick String Matcher")
-        self.root.geometry("825x675")
+        self.root.geometry("825x775")
         self.file_path = ""
         self.file_name = "File: "
+        self.create_background()
         self.construct_file_frm()
         self.construct_upload_button()
         self.construct_chosen_file()
@@ -40,6 +48,11 @@ class MainPage:
         self.output_content = []
         self.root.mainloop()
 
+    def create_background(self):
+        self.bg_image = PhotoImage(file='../img/bg.png')
+        self.bg_label = tk.Label(self.root, image=self.bg_image)
+        self.bg_label.place(relheight=1,relwidth=1)
+
     def construct_file_frm(self):
         self.file_frm = ttk.LabelFrame(self.root, padding=10, text="Upload JSON Here")
         self.file_frm.grid(row=0, column=0, padx=0, pady=20, ipadx=200, ipady=10)
@@ -49,7 +62,10 @@ class MainPage:
         self.output_frm.grid(row=2, column=0, padx=20, pady=20, ipadx=50)
 
     def construct_upload_button(self):
-        self.upload_button = ttk.Button(self.file_frm, text="Choose File", command=self.process_file)
+        upload_image = Image.open('../img/upload.png')
+        upload_image = upload_image.resize((50,50))
+        self.upload_image = ImageTk.PhotoImage(upload_image)
+        self.upload_button = tk.Button(self.file_frm, text="Choose File", command=self.process_file, image=self.upload_image, borderwidth=0)
         self.upload_button.grid(row=0, column=0, pady=20)
 
     def construct_chosen_file(self):
@@ -61,11 +77,17 @@ class MainPage:
         self.button_frm.grid(row=1, column=0, pady=20)
 
     def construct_solve_button(self):
-        self.solve_button = ttk.Button(self.button_frm, text="Solve", command=self.solve_file,padding=7)
+        solve_image = Image.open('../img/solve.png')
+        solve_image = solve_image.resize((125,50))
+        self.solve_image = ImageTk.PhotoImage(solve_image)
+        self.solve_button = tk.Button(self.button_frm, text="Solve", command=self.solve_file,image=self.solve_image,borderwidth=0)
         self.solve_button.pack(side="left", padx=5, pady=20)
 
     def construct_visualize_button(self):
-        self.visualize_button = ttk.Button(self.button_frm, text="Visualize", command=self.visualize_file,padding=7)
+        visualize_image = Image.open('../img/visu.png')
+        visualize_image = visualize_image.resize((125,50))
+        self.visualize_image = ImageTk.PhotoImage(visualize_image)
+        self.visualize_button = tk.Button(self.button_frm, text="Visualize", command=self.visualize_file,image=self.visualize_image,borderwidth=0)
         self.visualize_button.pack(side="left", padx=5, pady=20)
 
     def construct_text_output(self):
@@ -98,5 +120,3 @@ class MainPage:
     def visualize_file(self):
         LeGraph(self.solution_pack.to_graph)
 
-if __name__ == "__main__":
-    MainPage()
